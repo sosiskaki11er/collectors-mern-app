2fe794bd0fd0fa2998043ff515c99ba83af489d6
amir <amikamaymun@gmail.com>
2023-03-02 16:24:10 +0500

    fix: getposts fixed

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,19 +1,19 @@
 import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
-export const getPosts = async (req,res) => {
+export const getPosts = async (req, res) => {
     const { page } = req.query;
-
-    try{
+    
+    try {
         const LIMIT = 8;
         const startIndex = (Number(page) - 1) * LIMIT;
-        const total = await PostMessage.countDocuments({})
+    
+        const total = await PostMessage.countDocuments({});
+        const posts = await PostMessage.find().sort({ _id: -1 }).limit(LIMIT).skip(startIndex);
 
-        const posts = await PostMessage.find().sort({_id: -1}).limit(LIMIT).skip(startIndex);
-
-        res.status(200).json({ data: posts, currentPage: Number(page), numberOfPage: Math.ceil(total/ LIMIT) })
-    } catch(error) {
-        res.status(404).json({ message: error.message})
+        res.json({ data: posts, currentPage: Number(page), numberOfPages: Math.ceil(total / LIMIT)});
+    } catch (error) {    
+        res.status(404).json({ message: error.message });
     }
 }
 
